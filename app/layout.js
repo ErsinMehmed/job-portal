bbd9083b8fd43c8c93cbb881888cd7b4783725fb
Ersin Hyusein <ersin.hyusein@mypos.corp>
2023-10-19 15:44:16 +0300

    added vercel analitics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 // import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import Provider from "./provider";
 import { AuthProvider } from "./providerAuth";
 
@@ -13,10 +14,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
         <AuthProvider>
-          <Provider>{children}</Provider>
+          <Provider>
+            {children}
+            <Analytics />
+          </Provider>
         </AuthProvider>
       </body>
     </html>
