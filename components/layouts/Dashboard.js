70f48042a514a849f3941ef1d16fb3086898c71f
Ersin Hyusein <ersin.hyusein@mypos.corp>
2023-10-31 09:15:23 +0200

    remove page width from state in dashboard layout

--- components/layouts/Dashboard.js
+++ components/layouts/Dashboard.js
@@ -6,19 +6,8 @@ import { useSession } from "next-auth/react";
 import { RoleEnums } from "../../enums/role";
 
 const DashboardLayout = (props) => {
-  const [pageWidth, setPageWidth] = useState(null);
   const [isVisible, setIsVisible] = useState(false);
   const { data: session } = useSession();
-
-  useEffect(() => {
-    setPageWidth(window.innerWidth ?? null);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const handleResize = () => {
-    setPageWidth(window.innerWidth ?? null);
-  };
 
   const toggleMenu = () => {
     setIsVisible(!isVisible);
@@ -26,18 +15,15 @@ const DashboardLayout = (props) => {
 
   return (
     <div className="flex items-center w-full">
-      {pageWidth && pageWidth > 640 && <SideBar show={isVisible} />}
-      {pageWidth && pageWidth < 640 && <MobileMenu show={isVisible} />}
+      <SideBar show={isVisible} />
+      <MobileMenu show={isVisible} />
+
       <div
         className={`${
           isVisible ? "sm:ml-16" : "sm:ml-56 xl:ml-64 2xl:ml-72"
         } transition-all duration-500 w-full`}
       >
-        <Navbar
-          onMenuClick={toggleMenu}
-          pageWidth={pageWidth}
-          show={isVisible}
-        />
+        <Navbar onMenuClick={toggleMenu} show={isVisible} />
         {/* {RoleEnums.EMPLOYER === session.user.role && (
           <Navbar
             onMenuClick={toggleMenu}
