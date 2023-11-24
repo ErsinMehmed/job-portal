e8964b78ccf0635f88372f95390fc06031e98b26
Ersin Hyusein <ersin.hyusein@mypos.corp>
2023-11-24 17:52:21 +0200

    added route for ad getting

---
+++ app/api/ads/[id]/route.js
@@ -0,0 +1,13 @@
+import connectMongoDB from "@/libs/mongodb";
+import Ad from "@/models/ad";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const { id } = params;
+
+  await connectMongoDB();
+
+  const ad = await Ad.findOne({ _id: id });
+
+  return NextResponse.json({ ad }, { status: 200 });
+}
