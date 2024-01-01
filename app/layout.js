368849fa5202e93f994e8486dabc88ee977ce057
erso <ersin99mehmed@abv.bg>
2024-01-01 22:03:12 +0200

    added vercel speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Provider from "../providers/MainProvider";
 import { AuthProvider } from "../providers/AuthProvider";
 
@@ -10,16 +11,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <link rel="icon" sizes="32x32" href="/images/logo.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel='icon'
+          sizes='32x32'
+          href='/images/logo.svg'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
       </head>
       <body>
         <AuthProvider>
           <Provider>
             {children}
             <Analytics />
+            <SpeedInsights />
           </Provider>
         </AuthProvider>
       </body>
